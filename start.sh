608782fdb13dd114952c3d494c77014f355e17d7
Kadreev <kadreev@gmail.com>
2023-01-24 07:52:13 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+export N8N_USER_FOLDER=/home/root/n8n
+export N8N_CUSTOM_EXTENSIONS="/home/root/n8n/custom-nodes;/home/root/n8n/custom"
 # check if port variable is set or go with default
 if [ -z ${PORT+x} ]; then echo "PORT variable not defined, leaving N8N to default port."; else export N8N_PORT=$PORT; echo "N8N will start on '$PORT'"; fi
 
